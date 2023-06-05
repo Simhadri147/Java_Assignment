010d5e2fd882c5afab47161b5b32dd0e46d83715
YERRALALEPPAGARI SIMHADRI <134131578+Simhadri147@users.noreply.github.com>
2023-06-05 14:15:58 +0530

    Second Commit

--- BankAccManage.java
+++ BankAccManage.java
@@ -1,71 +1,77 @@
 import java.util.Scanner;
-
-public class BankAccountManager {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BankAccount account = new BankAccount();
-        
-        System.out.println("Welcome to the Bank Account Manager!");
-        
-        while (true) {
-            System.out.println("\n1. Check Balance");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Quit");
-            System.out.print("Enter your choice (1-4): ");
-            int choice = sc.nextInt();
-            
-            switch (choice) {
-                case 1:
-                    System.out.println("Your current balance is: " + account.getBalance());
-                    break;
-                case 2:
-                    System.out.print("Enter the amount to deposit: ");
-                    double depositAmount = sc.nextDouble();
-                    account.deposit(depositAmount);
-                    System.out.println("Amount deposited successfully.");
-                    break;
-                case 3:
-                    System.out.print("Enter the amount to withdraw: ");
-                    double withdrawAmount = sc.nextDouble();
-                    boolean withdrawSuccess = account.withdraw(withdrawAmount);
-                    if (withdrawSuccess) {
-                        System.out.println("Amount withdrawn successfully.");
-                    } else {
-                        System.out.println("Insufficient balance. Withdrawal failed.");
-                    }
-                    break;
-                case 4:
-                    System.out.println("Thank you for using Bank Account Manager. Goodbye!");
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
-    }
+class BankAcc
+{
+	Scanner sc = new Scanner(System.in);
+	double balance = 0.0;
+	public void displaybal()
+	{
+		System.out.print("Available balance:"+balance);
+	}
+	public void deposit()
+	{
+		System.out.println("Enter amount you want to deposit:");
+	double amount = sc.nextDouble();
+		balance = balance+amount;
+		System.out.println("Amount Deposited successfully");
+		System.out.println("Total balance:"+balance);
+	}
+	public void withdraw()
+	{
+		System.out.println("Enter amount you want to withdraw:");
+		double amount = sc.nextDouble();
+		if(amount <= balance)
+		{
+	 	balance=balance - amount;
+	 	System.out.println("Amount withdrawed successully");
+		}
+		else
+		 System.out.println(" Transaction failed due to insufficient balance ..!");
+		System.out.println("Available balance:"+balance);
+		 
+	}
+	public void exit()
+	{
+			System.out.println("Thanks for the visit.\n Have a nice day");
+			System.exit(0);
+	}
 }
-
-class BankAccount {
-    private double balance;
-
-    public BankAccount() {
-        this.balance = 0.0;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void deposit(double amount) {
-        balance += amount;
-    }
-
-    public boolean withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            return true;
-        } else {
-            return false;
-        }
-    }
+class Main 
+{
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+		BankAcc obj = new BankAcc();
+		while(true)
+		{
+		   System.out.println("\n welcome to the bank account manager :");
+	    System.out.println(" ");
+	    System.out.println("1.Balance Enquiry");
+	    System.out.println("2.Balance Deposit:");
+	    System.out.println("3. Balance withdraw");
+	    System.out.println("4.Quit");
+	    System.out.println(" ");
+	    System.out.println("Enter your choice(1 - 4):");
+	   
+	    int choice = sc.nextInt();
+	   	switch(choice)
+	    {
+	    	case 1: 
+	    	obj.displaybal();
+	    	break;
+	    	case 2:
+	        obj.deposit();
+	        break;
+	        case 3:
+	        obj.withdraw();
+	        break;
+	        case 4:
+	        obj.exit();
+	        break;
+	        default :
+	        System.out.println("Wrong choice try again.!!\n");
+	     }
+	    System.out.println("\n");
+	    }
+	}
 }
+	
